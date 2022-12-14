e51453a5296a79c15923d8bb36b26509a41a7e14
Ryan Null <ryan.null@gmail.com>
2022-12-14 14:42:35 -0800

    building stuff out

--- backend/models/session.py
+++ backend/models/session.py
@@ -3,6 +3,19 @@ from peewee import DateTimeField, UUIDField
 
 
 class Session(Base):
-    session = UUIDField(primary_key=True)
-    created = DateTimeField()
-    expires = DateTimeField()
+    """
+    Session model
+
+    Attributes:
+        id (UUIDField): Session ID
+        created_at (DateTimeField): Session creation date
+        updated_at (DateTimeField): Session update date
+        user_id (UUIDField): User ID
+    """
+    id = UUIDField(primary_key=True)
+    created_at = DateTimeField()
+    updated_at = DateTimeField()
+    user_id = UUIDField()
+
+    class Meta:
+        table_name = 'sessions'
--- backend/resources/auth.py
+++ backend/resources/auth.py
@@ -11,11 +11,24 @@ auth_resource = Blueprint(
 
 @auth_resource.route('/init', methods=['POST'])
 def init_sso_session():
+    """
+    Initializes a new VATSIM SSO session.
+
+    This endpoint will initialize a new VATSIM SSO session and redirect the user to the VATSIM SSO login page.
+
+    :return:
+    """
     return wrap_standard_error("Not implemented"), 500
 
 
 @auth_resource.route('/return')
 def handle_return_from_vatsim():
-    body = request.json
+    """
+    Function to handle the return from VATSIM SSO.
+
+    Upon returning from VATSIM SSO, the user will be redirected to this endpoint with a code in the query string.
+
+    The user will be logged in if the code is valid, otherwise they will be redirected to the login page with an error.
+    """
 
     return wrap_standard_error("Not implemented"), 500
